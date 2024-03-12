57544746c54c47f0bf59f96aae2feab825d0c588
sodik abdullaev <sodiqabdullaev@mail.ru>
2024-03-12 23:43:11 +0000

    view single article is complete

--- src/api.js
+++ src/api.js
@@ -1,10 +1,15 @@
 import axios from 'axios'
-const articleApi = axios.create({
+const newsApi = axios.create({
 	baseURL: 'https://nc-news-4wkm.onrender.com/api',
 })
 
 export function getArticles() {
-	return articleApi.get('/articles').then(({ data }) => {
+	return newsApi.get('/articles').then(({ data }) => {
 		return data.articles
+	})
+}
+export function getArticleById(article_id) {
+	return newsApi.get(`/articles/${article_id}`).then(({ data }) => {
+		return data.article
 	})
 }
--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -1,14 +1,14 @@
 function Breadcrumb({ crumbs }) {
 	return (
-		<nav class="flex" aria-label="Breadcrumb">
-			<ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-				<li class="inline-flex items-center">
+		<nav className="flex" aria-label="Breadcrumb">
+			<ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+				<li className="inline-flex items-center">
 					<a
 						href="/"
-						class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+						className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
 					>
 						<svg
-							class="w-3 h-3 me-2.5"
+							className="w-3 h-3 me-2.5"
 							aria-hidden="true"
 							xmlns="http://www.w3.org/2000/svg"
 							fill="currentColor"
@@ -21,9 +21,9 @@ function Breadcrumb({ crumbs }) {
 				</li>
 				{crumbs.map((crumb, index) => (
 					<li key={index}>
-						<div class="flex items-center">
+						<div className="flex items-center">
 							<svg
-								class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+								className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
 								aria-hidden="true"
 								xmlns="http://www.w3.org/2000/svg"
 								fill="none"
@@ -31,15 +31,15 @@ function Breadcrumb({ crumbs }) {
 							>
 								<path
 									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
 									d="m1 9 4-4-4-4"
 								/>
 							</svg>
 							<a
 								href={crumb.link}
-								class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white"
+								className="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white"
 							>
 								{crumb.title}
 							</a>
